ebfbc53fd7e88668f9d55b5990dcce7cb80b6039
NasimBilal <71866414+NasimBilal@users.noreply.github.com>
2020-10-25 12:19:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+# Python is Easy
+"""Homework #1
+    My First Assignment on Variables
+    """
+# song name
+Song = "Uyirin Nadhiye"
+
+# movie name
+Movie = "Mayanadhi"
+
+# composed by
+Composed = "Rex Vijayan"
+
+# duration
+Duration = 04.31
+
+# release year
+Year = 2019
+
+# Song Link
+Link = "https://youtu.be/ZmLxAakkH1Q"
+
+# Song Lyrics
+Lyrics = "Vinayak Sasikumar"
+
+# Released by
+Production = "OPM Mills Cinemas"
+
+print(Song)
+print(Movie)
+print(Composed)
+print(Duration)
+print(Year)
+print(Link)
+print(Lyrics)
+print(Production)
